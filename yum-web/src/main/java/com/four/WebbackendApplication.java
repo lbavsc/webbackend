5d245627833959d2d82ab02c835bf1904879def5
lbavsc <471242574@qq.com>
2021-07-05 12:38:06 +0800

    feat(构建多模块):
    1. yum-common        通用模块
    2. yum-web           web模块
    3. yum-user          用户模块
    4. yum-file          文件模块
    5. yum-share         分享模块
    6. yum-collect       收藏模块
    7. yum-trash         回收站模块

--- yum-web/src/main/java/com/four/WebbackendApplication.java
+++ yum-web/src/main/java/com/four/WebbackendApplication.java
@@ -1,8 +1,11 @@
-package com.four.webbackend;
+package com.four;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lbavsc
+ */
 @SpringBootApplication
 public class WebbackendApplication {
 
